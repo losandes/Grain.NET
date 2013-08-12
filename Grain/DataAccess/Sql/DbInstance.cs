da432e535361d4e1bbbf70c76f612271cd9f317b
&y <andes.collab@gmail.com>
2013-08-12 10:32:34 -0400

    Allows CommandBahavior to be set
    when executing commands against DbInstance. Also adds lots of missing
    comments.

--- Grain/DataAccess/Sql/DbInstance.cs
+++ Grain/DataAccess/Sql/DbInstance.cs
@@ -65,9 +65,9 @@ namespace Grain.DataAccess.Sql
         /// <param name="command">SqlCommand: the command to execute</param>
         /// <param name="modelBinder">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <returns>IEnumerable of type T: the result set bound to a model, by way of the selector factory.</returns>
-        public virtual T ExecuteAsSingle<T>(SqlCommand command, Func<IDataRecord, T> modelBinder)
+        public virtual T ExecuteAsSingle<T>(SqlCommand command, Func<IDataRecord, T> modelBinder, CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
-            var _result = ExecuteAs<T>(command, modelBinder);
+            var _result = ExecuteAs<T>(command, modelBinder, commandBehavior: commandBehavior);
 
             if (_result != null)
             {
@@ -87,14 +87,14 @@ namespace Grain.DataAccess.Sql
         /// <param name="command">SqlCommand: the command to execute</param>
         /// <param name="modelBinder">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <returns>IEnumerable of type T: the result set bound to a model, by way of the selector factory.</returns>
-        public virtual IEnumerable<T> ExecuteAs<T>(SqlCommand command, Func<IDataRecord, T> modelBinder)
+        public virtual IEnumerable<T> ExecuteAs<T>(SqlCommand command, Func<IDataRecord, T> modelBinder, CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             using (command)
             {
                 command.Connection = SqlConnection;
                 command.CommandTimeout = _defaultCommandTimeout;
 
-                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                using (SqlDataReader reader = command.ExecuteReader(commandBehavior))
                 {
                     while (reader.Read())
                         yield return modelBinder(reader);
@@ -119,16 +119,21 @@ namespace Grain.DataAccess.Sql
         /// <typeparam name="T1">Type: the type of object for the first result set</typeparam>
         /// <typeparam name="T2">Type: the type of object for the second result set</typeparam>
         /// <param name="command">SqlCommand: a stored procedure command</param>
+        /// <param name="modelBinder1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="commandBehavior">CommandBehavior: a description of the results of the query and its effect on the database (our default is CloseConnection)</param>
         /// <returns>List of Type T: the results in a collection</returns>
         public virtual void ExecuteAs<T1, T2>(SqlCommand command,
             Func<IDataRecord, T1> modelBinder1, List<T1> output1,
-            Func<IDataRecord, T2> modelBinder2, List<T2> output2)
+            Func<IDataRecord, T2> modelBinder2, List<T2> output2,
+            CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             ExecuteAs<T1, T2, object, object, object, object>(command,
                 modelBinder1: modelBinder1, output1: output1,
-                modelBinder2: modelBinder2, output2: output2);
+                modelBinder2: modelBinder2, output2: output2,
+                commandBehavior: commandBehavior);
         }
 
         /// <summary>
@@ -138,19 +143,25 @@ namespace Grain.DataAccess.Sql
         /// <typeparam name="T2">Type: the type of object for the second result set</typeparam>
         /// <typeparam name="T3">Type: the type of object for the third result set</typeparam>
         /// <param name="command">SqlCommand: a stored procedure command</param>
+        /// <param name="modelBinder1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder3">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output3">A list to store the output from the third result set (treat as ref: must be initialized)</param>
+        /// <param name="commandBehavior">CommandBehavior: a description of the results of the query and its effect on the database (our default is CloseConnection)</param>
         /// <returns>List of Type T: the results in a collection</returns>
         public virtual void ExecuteAs<T1, T2, T3>(SqlCommand command,
             Func<IDataRecord, T1> modelBinder1, List<T1> output1,
             Func<IDataRecord, T2> modelBinder2, List<T2> output2,
-            Func<IDataRecord, T3> modelBinder3, List<T3> output3)
+            Func<IDataRecord, T3> modelBinder3, List<T3> output3,
+            CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             ExecuteAs<T1, T2, T3, object, object, object>(command,
                 modelBinder1: modelBinder1, output1: output1,
                 modelBinder2: modelBinder2, output2: output2,
-                modelBinder3: modelBinder3, output3: output3);
+                modelBinder3: modelBinder3, output3: output3,
+                commandBehavior: commandBehavior);
         }
 
         /// <summary>
@@ -161,22 +172,29 @@ namespace Grain.DataAccess.Sql
         /// <typeparam name="T3">Type: the type of object for the third result set</typeparam>
         /// <typeparam name="T4">Type: the type of object for the fourth result set</typeparam>
         /// <param name="command">SqlCommand: a stored procedure command</param>
+        /// <param name="modelBinder1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder3">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output3">A list to store the output from the third result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder4">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output4">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="commandBehavior">CommandBehavior: a description of the results of the query and its effect on the database (our default is CloseConnection)</param>
         /// <returns>List of Type T: the results in a collection</returns>
         public virtual void ExecuteAs<T1, T2, T3, T4>(SqlCommand command,
             Func<IDataRecord, T1> modelBinder1, List<T1> output1,
             Func<IDataRecord, T2> modelBinder2, List<T2> output2,
             Func<IDataRecord, T3> modelBinder3, List<T3> output3,
-            Func<IDataRecord, T4> modelBinder4, List<T4> output4)
+            Func<IDataRecord, T4> modelBinder4, List<T4> output4,
+            CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             ExecuteAs<T1, T2, T3, T4, object, object>(command,
                 modelBinder1: modelBinder1, output1: output1,
                 modelBinder2: modelBinder2, output2: output2,
                 modelBinder3: modelBinder3, output3: output3,
-                modelBinder4: modelBinder4, output4: output4);
+                modelBinder4: modelBinder4, output4: output4,
+                commandBehavior: commandBehavior);
         }
 
         /// <summary>
@@ -188,25 +206,33 @@ namespace Grain.DataAccess.Sql
         /// <typeparam name="T4">Type: the type of object for the fourth result set</typeparam>
         /// <typeparam name="T5">Type: the type of object for the fourth result set</typeparam>
         /// <param name="command">SqlCommand: a stored procedure command</param>
+        /// <param name="modelBinder1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder3">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output3">A list to store the output from the third result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder4">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output4">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder5">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output5">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="commandBehavior">CommandBehavior: a description of the results of the query and its effect on the database (our default is CloseConnection)</param>
         /// <returns>List of Type T: the results in a collection</returns>
         public virtual void ExecuteAs<T1, T2, T3, T4, T5>(SqlCommand command,
             Func<IDataRecord, T1> modelBinder1, List<T1> output1,
             Func<IDataRecord, T2> modelBinder2, List<T2> output2,
             Func<IDataRecord, T3> modelBinder3, List<T3> output3,
             Func<IDataRecord, T4> modelBinder4, List<T4> output4,
-            Func<IDataRecord, T5> modelBinder5, List<T5> output5)
+            Func<IDataRecord, T5> modelBinder5, List<T5> output5,
+            CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             ExecuteAs<T1, T2, T3, T4, T5, object>(command,
                 modelBinder1: modelBinder1, output1: output1,
                 modelBinder2: modelBinder2, output2: output2,
                 modelBinder3: modelBinder3, output3: output3,
                 modelBinder4: modelBinder4, output4: output4,
-                modelBinder5: modelBinder5, output5: output5);
+                modelBinder5: modelBinder5, output5: output5,
+                commandBehavior: commandBehavior);
         }
 
         /// <summary>
@@ -219,12 +245,19 @@ namespace Grain.DataAccess.Sql
         /// <typeparam name="T5">Type: the type of object for the fourth result set</typeparam>
         /// <typeparam name="T6">Type: the type of object for the fourth result set</typeparam>
         /// <param name="command">SqlCommand: a stored procedure command</param>
+        /// <param name="modelBinder1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder3">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output3">A list to store the output from the third result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder4">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output4">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder5">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output5">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="modelBinder6">Func of type IDataRecord and T: the factory for binding the output to a model</param>
         /// <param name="output6">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="commandBehavior">CommandBehavior: a description of the results of the query and its effect on the database (our default is CloseConnection)</param>
         /// <returns>List of Type T: the results in a collection</returns>
         public virtual void ExecuteAs<T1, T2, T3, T4, T5, T6>(SqlCommand command,
             Func<IDataRecord, T1> modelBinder1 = null, List<T1> output1 = null,
@@ -232,7 +265,8 @@ namespace Grain.DataAccess.Sql
             Func<IDataRecord, T3> modelBinder3 = null, List<T3> output3 = null,
             Func<IDataRecord, T4> modelBinder4 = null, List<T4> output4 = null,
             Func<IDataRecord, T5> modelBinder5 = null, List<T5> output5 = null,
-            Func<IDataRecord, T6> modelBinder6 = null, List<T6> output6 = null)
+            Func<IDataRecord, T6> modelBinder6 = null, List<T6> output6 = null,
+            CommandBehavior commandBehavior = CommandBehavior.CloseConnection)
         {
             if (command == null)
                 throw new ArgumentException("The command cannot be null when trying to execute a SqlCommand", "command");
@@ -242,7 +276,7 @@ namespace Grain.DataAccess.Sql
                 command.Connection = SqlConnection;
                 command.CommandTimeout = _defaultCommandTimeout;
 
-                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                using (SqlDataReader reader = command.ExecuteReader(commandBehavior))
                 {
                     reader.ReadRecordsTo<T1, T2, T3, T4, T5, T6>(
                         factory1: modelBinder1, out1: output1,
@@ -270,12 +304,26 @@ namespace Grain.DataAccess.Sql
 
     public static class DbInstanceExtensions
     {
+        /// <summary>
+        /// Get the value or default(T) from a given IDataRecord (row) with the given column name
+        /// </summary>
+        /// <typeparam name="T">The type of output</typeparam>
+        /// <param name="row">IDataRecord: the row that is being parsed</param>
+        /// <param name="columnName">string: the column name for which to parse data from</param>
+        /// <returns>Type of T: the value for the resulting field, as T</returns>
         [Cite(Author = "Sky Sanders", Link = "http://skysanders.net/subtext/archive/2010/03/02/generic-nullsafe-idatarecord-field-getter.aspx", Type = CiteType.Adaptation)]
-        public static T GetValueOrDefault<T>(this IDataRecord row, string fieldName)
+        public static T GetValueOrDefault<T>(this IDataRecord row, string columnName)
         {
-            return row.GetValueOrDefault<T>(row.GetOrdinal(fieldName));
+            return row.GetValueOrDefault<T>(row.GetOrdinal(columnName));
         }
 
+        /// <summary>
+        /// Get the value or default(T) from a given IDataRecord (row) by column ordinal
+        /// </summary>
+        /// <typeparam name="T">The type of output</typeparam>
+        /// <param name="row">IDataRecord: the row that is being parsed</param>
+        /// <param name="ordinal">int: the ordinal of the column</param>
+        /// <returns>Type of T: the value for the resulting field, as T</returns>
         [Cite(Author = "Sky Sanders", Link = "http://skysanders.net/subtext/archive/2010/03/02/generic-nullsafe-idatarecord-field-getter.aspx", Type = CiteType.Copy)]
         public static T GetValueOrDefault<T>(this IDataRecord row, int ordinal)
         {
@@ -295,6 +343,25 @@ namespace Grain.DataAccess.Sql
                 output.Add(modelBinder(reader));
         }
 
+        /// <typeparam name="T1">Type: the type of object for the first result set</typeparam>
+        /// <typeparam name="T2">Type: the type of object for the second result set</typeparam>
+        /// <typeparam name="T3">Type: the type of object for the third result set</typeparam>
+        /// <typeparam name="T4">Type: the type of object for the fourth result set</typeparam>
+        /// <typeparam name="T5">Type: the type of object for the fourth result set</typeparam>
+        /// <typeparam name="T6">Type: the type of object for the fourth result set</typeparam>
+        /// <param name="reader">IDataReader: the open DataReader</param>
+        /// <param name="factory1">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out1">A list to store the output from the first result set (treat as ref: must be initialized)</param>
+        /// <param name="factory2">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out2">A list to store the output from the second result set (treat as ref: must be initialized)</param>
+        /// <param name="factory3">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out3">A list to store the output from the third result set (treat as ref: must be initialized)</param>
+        /// <param name="factory4">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out4">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="factory5">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out5">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
+        /// <param name="factory6">Func of type IDataRecord and T: the factory for binding the output to a model</param>
+        /// <param name="out6">A list to store the output from the fourth result set (treat as ref: must be initialized)</param>
         public static void ReadRecordsTo<T1, T2, T3, T4, T5, T6>(this IDataReader reader,
             Func<IDataRecord, T1> factory1 = null, List<T1> out1 = null,
             Func<IDataRecord, T2> factory2 = null, List<T2> out2 = null,
