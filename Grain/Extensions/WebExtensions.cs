07253cea3a0bc4db9105259b0fcccfe41c914ec9
&y <andes.collab@gmail.com>
2014-01-31 00:15:49 -0500

    Adds QueryString extension

---
+++ Grain/Extensions/WebExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Grain.Extensions
+{
+    public static class WebExtensions
+    {
+        public static NameValueCollection GetQueryStringCollection(HttpContext context)
+        {
+            return HttpUtility.ParseQueryString(context.Request.Url.Query.Substring(1));
+        }
+    }
+}
