4e813edc230eb56b1b1d85a2b7d091f259cf9907
&y <andes.collab@gmail.com>
2014-07-12 14:59:42 -0400

    Buildable - Tests Passing
    I removed missing Encryption libs and made db tests configurable.

--- Grain.Tests/Serialization/XmlSerializerTests.cs
+++ Grain.Tests/Serialization/XmlSerializerTests.cs
@@ -13,41 +13,9 @@ namespace Grain.Tests.Serialization
     {
         #region Setup
 
-        string _testModelXml = @"<TestModel xmlns:i=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://schemas.datacontract.org/2004/07/Grain.Tests.Models.TestModels"">
-  <Description>Foo Bar</Description>
-  <Id>1</Id>
-  <Name>Foo</Name>
-  <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>
-</TestModel>";
+        string _testModelXml = "<TestModel xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.datacontract.org/2004/07/Grain.Tests.Models.TestModels\">\r\n  <Description>Foo Bar</Description>\r\n  <Id>1</Id>\r\n  <Name>Foo</Name>\r\n  <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>\r\n</TestModel>";
 
-        string _testModelWithCollection = @"<TestModelWithCollections xmlns:i=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://schemas.datacontract.org/2004/07/Grain.Tests.Models.TestModels"">
-  <Description>Hello Bar</Description>
-  <Id>2</Id>
-  <Name>Hello</Name>
-  <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>
-  <Children>
-    <TestModel>
-      <Description>Foo Bar</Description>
-      <Id>3</Id>
-      <Name>Foo</Name>
-      <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>
-    </TestModel>
-  </Children>
-  <Dictionary xmlns:d2p1=""http://schemas.microsoft.com/2003/10/Serialization/Arrays"">
-    <d2p1:KeyValueOfstringstring>
-      <d2p1:Key>Foo</d2p1:Key>
-      <d2p1:Value>Bar</d2p1:Value>
-    </d2p1:KeyValueOfstringstring>
-    <d2p1:KeyValueOfstringstring>
-      <d2p1:Key>Hello</d2p1:Key>
-      <d2p1:Value>World</d2p1:Value>
-    </d2p1:KeyValueOfstringstring>
-  </Dictionary>
-  <StringCollection xmlns:d2p1=""http://schemas.microsoft.com/2003/10/Serialization/Arrays"">
-    <d2p1:string>Foo</d2p1:string>
-    <d2p1:string>Bar</d2p1:string>
-  </StringCollection>
-</TestModelWithCollections>";
+        string _testModelWithCollection = "<TestModelWithCollections xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.datacontract.org/2004/07/Grain.Tests.Models.TestModels\">\r\n  <Description>Hello Bar</Description>\r\n  <Id>2</Id>\r\n  <Name>Hello</Name>\r\n  <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>\r\n  <Children>\r\n    <TestModel>\r\n      <Description>Foo Bar</Description>\r\n      <Id>3</Id>\r\n      <Name>Foo</Name>\r\n      <TimeCreated>2012-12-04T14:50:06.4559307-05:00</TimeCreated>\r\n    </TestModel>\r\n  </Children>\r\n  <Dictionary xmlns:d2p1=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">\r\n    <d2p1:KeyValueOfstringstring>\r\n      <d2p1:Key>Foo</d2p1:Key>\r\n      <d2p1:Value>Bar</d2p1:Value>\r\n    </d2p1:KeyValueOfstringstring>\r\n    <d2p1:KeyValueOfstringstring>\r\n      <d2p1:Key>Hello</d2p1:Key>\r\n      <d2p1:Value>World</d2p1:Value>\r\n    </d2p1:KeyValueOfstringstring>\r\n  </Dictionary>\r\n  <StringCollection xmlns:d2p1=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">\r\n    <d2p1:string>Foo</d2p1:string>\r\n    <d2p1:string>Bar</d2p1:string>\r\n  </StringCollection>\r\n</TestModelWithCollections>";
 
         #endregion Setup
 
@@ -118,20 +86,7 @@ namespace Grain.Tests.Serialization
             _expected.Number = 42;
             _expected.Date = new DateTime(2013, 8, 3);
 
-            string _expectedXml = @"<ArrayOfKeyValueOfstringanyType xmlns:i=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://schemas.microsoft.com/2003/10/Serialization/Arrays"">
-  <KeyValueOfstringanyType>
-    <Key>Foo</Key>
-    <Value xmlns:d3p1=""http://www.w3.org/2001/XMLSchema"" i:type=""d3p1:string"">Bar</Value>
-  </KeyValueOfstringanyType>
-  <KeyValueOfstringanyType>
-    <Key>Number</Key>
-    <Value xmlns:d3p1=""http://www.w3.org/2001/XMLSchema"" i:type=""d3p1:int"">42</Value>
-  </KeyValueOfstringanyType>
-  <KeyValueOfstringanyType>
-    <Key>Date</Key>
-    <Value xmlns:d3p1=""http://www.w3.org/2001/XMLSchema"" i:type=""d3p1:dateTime"">2013-08-03T00:00:00</Value>
-  </KeyValueOfstringanyType>
-</ArrayOfKeyValueOfstringanyType>";
+            string _expectedXml = "<ArrayOfKeyValueOfstringanyType xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">\r\n  <KeyValueOfstringanyType>\r\n    <Key>Foo</Key>\r\n    <Value xmlns:d3p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d3p1:string\">Bar</Value>\r\n  </KeyValueOfstringanyType>\r\n  <KeyValueOfstringanyType>\r\n    <Key>Number</Key>\r\n    <Value xmlns:d3p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d3p1:int\">42</Value>\r\n  </KeyValueOfstringanyType>\r\n  <KeyValueOfstringanyType>\r\n    <Key>Date</Key>\r\n    <Value xmlns:d3p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d3p1:dateTime\">2013-08-03T00:00:00</Value>\r\n  </KeyValueOfstringanyType>\r\n</ArrayOfKeyValueOfstringanyType>";
             string _actualXml = Grain.Serialization.XMLSerializer.ToXml(_expected); //Grain.Serialization.XMLSerializer.ToXml(_expected);
             Assert.AreEqual(_expectedXml, _actualXml);
 
